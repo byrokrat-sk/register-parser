fd2445ef383ac70ac1213a2a21368aaddbf5b4aa
Martin Sršeň <martin@srsen.sk>
2020-04-20 01:55:26 +0200

    Added edgecase subject to test

--- SkGovernmentParser/dev.php
+++ SkGovernmentParser/dev.php
@@ -11,8 +11,12 @@ use \SkGovernmentParser\DataSources\BusinessRegister\Query\IdentificatorQuery;
 
 echo ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
+// TODO: Support foreign subjects:
 // const GOOGLE_SK_ICO = "45947597";
+// const ALLRISK_ICO = "35 947 501";
+
 const FINGO_SK_ICO = "50230859";
 const SOFTEC_ICO = "00683540";
 const PPC_ICO = "31561802";
+
 echo(json_encode(IdentificatorQuery::queryBy(PPC_ICO), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
