34a5962ad311e909f9e486c8f40cf04678483b75
Martin Sršeň <martin@srsen.sk>
2020-10-28 00:58:22 +0100

    Added error handler to turn notices into exceptions

--- dev.php
+++ dev.php
@@ -2,6 +2,15 @@
 
 error_reporting(-1);
 
+function exception_error_handler($severity, $message, $file, $line) {
+    if (!(error_reporting() & $severity)) {
+        // This error code is not included in error_reporting
+        return;
+    }
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
+set_error_handler("exception_error_handler");
+
 // Global debug function
 function dd($val, $json = false) {
     if ($json) {
